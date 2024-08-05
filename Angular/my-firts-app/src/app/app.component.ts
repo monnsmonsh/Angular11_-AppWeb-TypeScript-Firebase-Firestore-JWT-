6da8f9442b9c349c670d556d106f66a7333580ef
Martín Rodríguez <monse.developer@gmail.com>
2024-08-05 17:13:53 -0600

    up firstapp

--- Angular/my-firts-app/src/app/app.component.ts
+++ Angular/my-firts-app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-firts-app';
+  title = 'Mi Primera Aplicacion en Angular';
 }
